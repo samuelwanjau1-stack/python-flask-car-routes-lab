0e8667d7e0ca669983ca908eeac2476237634152
Your Full Name <samuel.wanjau1@student.moringaschool.com>
2026-04-18 12:59:55 +0300

    Final implementation of car routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,20 @@
-existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+from flask import Flask
+
+app = Flask(__name__)
+
+# The required model list
+existing_models = ["sedan", "suv", "truck", "coupe"]
+
+@app.route('/')
+def home():
+    return "Welcome to Flatiron Cars"
+
+@app.route('/<model>')
+def get_model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+if __name__ == '__main__':
+    app.run(port=5555) # Using 5555 is standard for these labs
