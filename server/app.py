f722b671f00731093d246bd5c285d3e4c29f694b
Your Full Name <samuel.wanjau1@student.moringaschool.com>
2026-04-18 13:14:37 +0300

    Final implementation of car routes

--- server/app.py
+++ server/app.py
@@ -13,4 +13,9 @@ def get_model(model):
     if model in existing_models:
         return f"Flatiron {model} is in our fleet!"
     else:
-        return f"No models called {model} exists in our catalog", 404
+       
+        return f"No models called {model} exists in our catalog"
+
+if __name__ == '__main__':
+    #
+    app.run(port=5555)
